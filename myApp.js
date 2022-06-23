6d83626579259d44f2b6c3032d6d12c39b61e831
Snawszer <zachary831@gmail.com>
2022-06-23 03:09:52 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,10 +4,8 @@ let app = express();
 console.log("Hello world");
 
 app.get('/', function(req, res) {
-    res.send("Hello Express"));
-}
-
-
+    res.send(__dirname + /views/index.html);
+});
 
 
 
