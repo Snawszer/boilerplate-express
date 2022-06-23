336a42ac32e2f7f81eef44d5ff3704a1d8dd38db
Snawszer <zachary831@gmail.com>
2022-06-23 03:30:41 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,13 +3,16 @@ let app = express();
 
 console.log("Hello world");
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use("/public",express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
+    res.json({"message": "Hello json"});
 });
 
-
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"});
+});
 
 
 
