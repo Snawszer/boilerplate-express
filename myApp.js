4dab4d15c1fa4d6d8803f637178d587a3a7e56cc
Snawszer <zachary831@gmail.com>
2022-06-25 04:10:54 -0300

    Middleware

--- myApp.js
+++ myApp.js
@@ -1,17 +1,27 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello world");
 
 app.use("/public",express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    next();
+});
+
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
-    res.json({"message": "Hello json"});
+    
 });
 
 app.get('/json', function(req, res) {
-    res.json({"message": "Hello json"});
+    if(process.env.MESSAGE_STYLE=== uppercase){
+        res.json({"message": "HELLO JSON"});
+    } else res.json({"message": "Hello json"});
+    
 });
 
 
